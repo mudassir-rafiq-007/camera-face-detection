c0b49f74bcc36f9b3ad55de61b03c5739a170041
Mudassir Rafiq <mudassir.rafiq.007@gmail.com>
2023-07-01 19:25:52 +0500

    Camera face detection not working with eas builds

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import * as ExpoCamera from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
 
 export default function App() {
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const [micPermission, setMicPermission] = useState(false);
+
+  function requestPermissions() {
+    ExpoCamera.requestCameraPermissionsAsync().then((response) => {
+      if (response.granted) {
+        setCameraPermission(true);
+        ExpoCamera.requestMicrophonePermissionsAsync().then((response) => {
+          if (response.granted) {
+            setMicPermission(true);
+          }
+        });
+      }
+    });
+  }
+
+  useEffect(() => {
+    requestPermissions();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {micPermission && cameraPermission ? (
+        <ExpoCamera.Camera
+          style={{ flex: 1 }}
+          type={ExpoCamera.CameraType.front}
+          onFacesDetected={(faces) => console.log(faces)}
+          faceDetectorSettings={{
+            mode: FaceDetector.FaceDetectorMode.accurate,
+            detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+            runClassifications: FaceDetector.FaceDetectorClassifications.none,
+            tracking: true,
+          }}
+        ></ExpoCamera.Camera>
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text>Permission not granted</Text>
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
